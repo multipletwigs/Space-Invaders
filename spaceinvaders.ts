856202b694813d7fb5c215ca380536c98f96532c
multipletwigs <lappy.hoe@gmail.com>
2021-09-01 09:59:00 +0800

    Added Shoot Class

--- spaceinvaders.ts
+++ spaceinvaders.ts
@@ -3,12 +3,21 @@ import {map, filter, scan, merge, reduce} from 'rxjs/operators';
 
 function spaceinvaders() {
   type Event = "keydown" | "keyup"
-  type Key = "a" | "d"
+  type Key = "a" | "d" | "w"
+
   const constants = {
-    CanvasSize: 600
-  }
+    ShipStartPos: {x: 253, y:500},
+    CanvasSize: 600,
+    ShipVelocity: 1,
+    StartTime: 0,
+    BulletExpirationTime: 1000, 
+    BulletRadius: 3, 
+    BulletVelocity: 2
+  } as const 
+
   class Tick {constructor(public readonly elapsed: number) {}}
   class Motion {constructor(public readonly direction: number) {}}
+  class Shoot{constructor(){}}
 
   class LinearMotion{
   constructor(public readonly x: number = 0, public readonly y: number = 0){}
@@ -19,14 +28,37 @@ function spaceinvaders() {
   static Zero = new LinearMotion();
 }
 
+  type ObjectID = Readonly<{
+    id: string, 
+    createTime: number 
+  }>
+
+  interface Ship {
+    pos: LinearMotion, 
+    direction: number 
+  }
+
+  interface gameObjects extends ObjectID{
+    pos: LinearMotion,
+    velocity: number
+  }
+
   type State = Readonly<{
-    pos: LinearMotion
-    direction: number
+    time: number,
+    ship: Ship
+    bullets: ReadonlyArray<gameObjects>,
+    exit: ReadonlyArray<gameObjects>, 
+    objCount: number, 
+    gameOver: boolean  
   }>
 
   const initialState: State = {
-    pos: new LinearMotion(300, 500), //Initial Position
-    direction: 0
+    time: 0,
+    ship: {pos: new LinearMotion(constants.ShipStartPos.x, constants.ShipStartPos.y), direction: 0}, //Initial Position
+    bullets: [],
+    exit: [], 
+    objCount: 0, 
+    gameOver: false
   }
 
   const observeKey = <T>(e: Event, k: Key, result: () => T) => 
@@ -35,10 +67,12 @@ function spaceinvaders() {
                        filter(({repeat}) => !repeat), 
                        map(result)
                      )
-  const startLeftMove = observeKey('keydown', 'a', () => new Motion(-1))
-  const startRightMove = observeKey('keydown', 'd', () => new Motion(1))
+                     
+  const startLeftMove = observeKey('keydown', 'a', () => new Motion(-constants.ShipVelocity))
+  const startRightMove = observeKey('keydown', 'd', () => new Motion(constants.ShipVelocity))
   const stopLeftMove = observeKey('keyup', 'a', () => new Motion(0))
   const stopRightMove = observeKey('keyup', 'd', () => new Motion(0))
+  const shoot = observeKey('keydown', 'w', ()=>new Shoot())
 
   function wrapAround({x, y}: LinearMotion): LinearMotion{
     const size = constants.CanvasSize 
@@ -50,23 +84,23 @@ function spaceinvaders() {
   const reduceState = (s: State, e: Motion|Tick) => 
     e instanceof Motion ? {
       ...s, 
-      direction: e.direction
+      ship: {...s.ship, direction: e.direction}
     } : {
       ...s, 
-      pos: wrapAround(s.pos.add(new LinearMotion(s.direction, 0)))
+      ship: {...s.ship, pos: wrapAround(s.ship.pos.add(new LinearMotion(s.ship.direction, 0)))}
     }
   
   const subscription = interval(10).pipe(
     map(elapsed => new Tick(elapsed)),
     merge(
-      startLeftMove, startRightMove, stopLeftMove, stopRightMove
+      startLeftMove, startRightMove, stopLeftMove, stopRightMove, shoot
     ),
     scan(reduceState, initialState))
     .subscribe(updateView)
 
   function updateView(s: State){
     const ship = document.getElementById("ship")!
-    ship.setAttribute('transform', `translate(${s.pos.x}, ${s.pos.y}) matrix(0.15038946 0 0 0.15038946 12.499998 -0)`)
+    ship.setAttribute('transform', `translate(${s.ship.pos.x}, ${s.ship.pos.y}) matrix(0.15038946 0 0 0.15038946 12.499998 -0)`)
   }
   
 
