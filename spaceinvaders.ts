8bc681a694a1b6a31a7aac6a47c3b0b647102016
multipletwigs <lappy.hoe@gmail.com>
2021-09-03 11:17:39 +0800

    Added Collision

--- spaceinvaders.ts
+++ spaceinvaders.ts
@@ -1,5 +1,5 @@
 import {fromEvent, interval} from 'rxjs';
-import {map, filter, scan, merge, reduce} from 'rxjs/operators'; 
+import {map, filter, scan, merge, reduce, mergeMap, concatMap} from 'rxjs/operators'; 
 
 
 /* Things TO-DO 
@@ -12,17 +12,22 @@ import {map, filter, scan, merge, reduce} from 'rxjs/operators';
 function spaceinvaders() {
   type Event = "keydown" | "keyup"
   type Key = "a" | "d" | "w"
+  type ViewType = "alienBullet" | "shipBullet" | "alien" | "ship"
 
   const constants = {
-    ShipStartPos: {x: 253, y:500},
-    CanvasSize: 600,
-    ShipVelocity: 1,
-    AlienVelocity: 0.25, 
-    StartTime: 0,
-    StartAlienCount: 3,
+    AlienVelocity: 0.1, 
+    AlienWidth: 30,
+    AlienHeight: 10,
+    AlienColumns: 4, 
+    AlienRows: 4,
     BulletExpirationTime: 100, 
     BulletRadius: 3, 
-    BulletVelocity: 4
+    BulletVelocity: 4,
+    CanvasSize: 600,
+    StartTime: 0,
+    StartAlienCount: 16,
+    ShipStartPos: {x: 253, y:500},
+    ShipVelocity: 1
   } as const 
 
   class Tick {constructor(public readonly elapsed: number) {}}
@@ -33,7 +38,7 @@ function spaceinvaders() {
   constructor(public readonly x: number = 0, public readonly y: number = 0){}
   add = (b: LinearMotion) => new LinearMotion(this.x + b.x, this.y + b.y);
   sub = (b: LinearMotion) => this.add(b.scale(-1));
-  scale = (s: number) => new LinearMotion(this.x * s, this.y * s); 
+  scale = (s: number) => new LinearMotion(this.x * s, this.y * s);
 
   static Zero = new LinearMotion();
  }
@@ -43,20 +48,18 @@ function spaceinvaders() {
     createTime: number 
   }>
 
-  interface Ship {
-    pos: LinearMotion, 
-    direction: number 
-  }
-
-  interface gameObjects extends ObjectID{
+  interface gameObjectsI extends ObjectID {
     pos: LinearMotion,
     velocity: number
   }
 
+  type gameObjects = Readonly<gameObjectsI>
+
   type State = Readonly<{
     time: number,
-    ship: Ship
-    bullets: ReadonlyArray<gameObjects>,
+    ship: gameObjects,
+    shipBullets: ReadonlyArray<gameObjects>,
+    alienBullets: ReadonlyArray<gameObjects>,
     exit: ReadonlyArray<gameObjects>, 
     aliens: ReadonlyArray<gameObjects>,
     objCount: number, 
@@ -65,14 +68,22 @@ function spaceinvaders() {
     score: number
   }>
 
-  const createAliens = () => [1,2,3,4].map(i => <gameObjects>{id: "Alien" + String(i), pos: new LinearMotion(100 + i * 50, 100), velocity: constants.AlienVelocity, createTime: 0})
+  const createAliens = (vT: ViewType) => (rows: number) => (columns: number) => (velocity: number) => [...Array(constants.StartAlienCount).keys()].map(
+    (val, index) => 
+    ({
+      id: String(Math.floor(val/rows)) + String(index % columns) + vT,
+      createTime: 0,
+      pos: new LinearMotion(100 + index % columns * 100, 100 + Math.floor(val/rows) * 50),
+      velocity: velocity
+    }))
 
   const initialState: State = {
     time: 0,
-    ship: {pos: new LinearMotion(constants.ShipStartPos.x, constants.ShipStartPos.y), direction: 0}, //Initial Position
-    bullets: [],
+    ship: {id: "playerShip", pos: new LinearMotion(constants.ShipStartPos.x, constants.ShipStartPos.y), velocity: 0, createTime: 0}, //Initial Position
+    shipBullets: [],
+    alienBullets: [], 
     exit: [], 
-    aliens: createAliens(), 
+    aliens: createAliens("alien")(constants.AlienColumns)(constants.AlienRows)(constants.AlienVelocity), 
     objCount: 0, 
     gameOver: false,
     level: 1,
@@ -102,33 +113,38 @@ function spaceinvaders() {
   const reduceState = (s: State, e: Motion|Tick|Shoot) => 
     e instanceof Motion ? {
       ...s, 
-      ship: {...s.ship, direction: e.direction}
+      ship: {...s.ship, velocity: e.direction}
     } : 
     e instanceof Shoot? {
       ...s, 
-      bullets: s.bullets.concat([{id: String(s.objCount), createTime: s.time, pos: s.ship.pos, velocity: constants.BulletVelocity}]),
+      shipBullets: s.shipBullets.concat([{id: String(s.objCount), createTime: s.time, pos: s.ship.pos, velocity: constants.BulletVelocity}]),
       objCount: s.objCount + 1
     } : tick(s, e.elapsed)
+
+  function collisionCheck([a, b] : [gameObjects, gameObjects]): boolean{
+    return a.pos.x < b.pos.x + constants.AlienWidth && a.pos.y < b.pos.y + constants.AlienHeight && a.pos.x + constants.AlienWidth > b.pos.x && a.pos.y + constants.AlienHeight > b.pos.y
+  }
     
   
   const tick = (s:State, elapsed: number) => {
     const expired = (g: gameObjects) => (elapsed - g.createTime) > constants.BulletExpirationTime,
     notExpired = (g: gameObjects) => (elapsed - g.createTime) <= constants.BulletExpirationTime,
     moveAliens = (g: gameObjects) => (elapsed) > 300 ? {...g, pos: new LinearMotion(g.pos.x, g.pos.y + g.velocity)} :  {...g, pos: new LinearMotion(g.pos.x + g.velocity, g.pos.y)},
-    removeAlien = (g: gameObjects) => g.pos.y > 300, 
 
-    activeBullets:gameObjects[] = s.bullets.filter(notExpired),
-    activeAliens: gameObjects[] = s.aliens.map(moveAliens),
-    expiredBullets:gameObjects[] = s.bullets.filter(expired),
-    expiredAlien: gameObjects[] = s.aliens.filter(removeAlien)
-
+    activeBullets:gameObjects[] = s.shipBullets.filter(notExpired),
+    expiredBullets:gameObjects[] = s.shipBullets.filter(expired),
+    allBulletsAndAliens = flatMap(s.shipBullets, b=> s.aliens.map<[gameObjects, gameObjects]>(r=>([b,r]))),
+    collidedBulletsAndAliens = allBulletsAndAliens.filter(collisionCheck),
+    collidedBullets = collidedBulletsAndAliens.map(([bullet,_])=>bullet),
+    collidedAliens = collidedBulletsAndAliens.map(([_,aliens])=>aliens)
+    
     return {
       ...s,
       time: elapsed,
-      ship:{...s.ship, pos: wrapAround(s.ship.pos.add(new LinearMotion(s.ship.direction, 0)))},
-      bullets: activeBullets.map(bulletMove), 
-      aliens: activeAliens,
-      exit: expiredBullets.concat(expiredAlien)
+      ship:{...s.ship, pos: wrapAround(s.ship.pos.add(new LinearMotion(s.ship.velocity, 0)))},
+      shipBullets: activeBullets.filter(n => !collidedBullets.includes(n)).map(bulletMove), 
+      exit: expiredBullets.concat(collidedBullets, collidedAliens),
+      aliens: s.aliens.filter(n => !collidedAliens.includes(n)).map(moveAliens)
     }
   }
 
@@ -146,6 +162,7 @@ function spaceinvaders() {
     .subscribe(updateView)
 
   function updateView(s: State){
+    console.log(s.aliens)
     const ship = document.getElementById("ship")!
     const svg = document.getElementById("canvas")
     ship.setAttribute('transform', `translate(${s.ship.pos.x}, ${s.ship.pos.y}) matrix(0.15038946 0 0 0.15038946 12.499998 -0)`)
@@ -165,31 +182,47 @@ function spaceinvaders() {
       v.setAttribute("cx", `${b.pos.x + 50}`) //50 to offset from ship position, do not want to ruin other places values
       v.setAttribute("cy", `${b.pos.y}`)
     };
-    s.bullets.forEach(updateBulletView)
+    s.shipBullets.forEach(updateBulletView)
 
     const updateAlienView = (b: gameObjects) => {
     function createAlienView(){
-      const v = document.createElementNS(svg.namespaceURI, "ellipse")!;
+      const v = document.createElementNS(svg.namespaceURI, "rect")!;
+      //Can use Objects.Entries
       v.setAttribute("id", `${b.id}`)
-      v.setAttribute("rx", `${constants.BulletRadius}`)
-      v.setAttribute("ry", `${constants.BulletRadius}`)
+      v.setAttribute("width", `${constants.AlienWidth}`)
+      v.setAttribute("height", `${constants.AlienHeight}`)
       v.setAttribute("fill", "white")
-      v.classList.add("Bullets")
+      v.classList.add("Aliens")
       svg.appendChild(v)
       return v
     }
-    const v = document.getElementById(b.id) || createAlienView();
-    v.setAttribute("cx", `${b.pos.x}`) //50 to offset from ship position, do not want to ruin other places values
-    v.setAttribute("cy", `${b.pos.y}`)
+    const v = document.getElementById(b.id) || createAlienView() ;
+    v.setAttribute("x", `${b.pos.x}`) 
+    v.setAttribute("y", `${b.pos.y}`)
     };
 
     s.aliens.forEach(updateAlienView)
 
     s.exit.map(o=>document.getElementById(o.id))
           .filter((item) => item !== null || undefined) //isNotNullorUndefined
-          .forEach(v => svg.removeChild(v))
+          .forEach(v=>{
+            try {
+              svg.removeChild(v)
+            } catch(e) {
+              // rarely it can happen that a bullet can be in exit 
+              // for both expiring and colliding in the same tick,
+              // which will cause this exception
+              console.log("Already removed: "+v.id)
+            }
+          })
   }
   
+  function flatMap<T,U>(
+    a:ReadonlyArray<T>,
+    f:(a:T)=>ReadonlyArray<U>
+  ): ReadonlyArray<U> {
+    return Array.prototype.concat(...a.map(f));
+  }
 
 }
   
